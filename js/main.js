2fcc55232c564874da62633e339dc16b4d36f53b
Osama A. <osamafathy711@gmail.com>
2025-05-19 09:11:07 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,14 +57,14 @@ document.addEventListener('DOMContentLoaded', function () {
         path: 'https://assets6.lottiefiles.com/packages/lf20_qp1q7mct.json'
     });
 
-    emailjs.init("gY5AMjqFRudW68mvX");
+    emailjs.init("YDb5bj1zjDrfbBB46");
 
     const form = document.getElementById("contact-form");
     if (form) {
         form.addEventListener("submit", function (e) {
             e.preventDefault();
 
-            emailjs.sendForm("service_wdob337", "template_jxqg9jy", form)
+            emailjs.sendForm("service_ugdl30e", "template_vdhlxum", form)
                 .then(function () {
                     alert("✅ Your message has been sent successfully. Thank you!");
                     form.reset();
